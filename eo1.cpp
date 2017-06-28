a3c37b9345a31dd5f635f8eb4a28938e65557494
jayapriyakn <jaya.priya1987kn@gmail.com>
2017-06-28 13:01:46 +0530

    odd or even

---
+++ eo1.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+cout<<"Enter the number to check whether it is odd or even";
+cin>>num;
+if(num%2==0)
+{
+cout<<"The number is even";
+else
+cout<<"The number is odd";
+}
+getch();
+}
