ac967a574939827bb7a39c689f78caf802b3b996
jayapriyakn <jaya.priya1987kn@gmail.com>
2017-06-28 16:54:43 +0530

    swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+cout<<"Enter the value of a";
+cin>>a;
+cout<<"Enter the value of b";
+cin>>b;
+c=a;
+a=b;
+b=c;
+cout<<"After swap a: "<<a<<"b: "<<b;
+getch();
+}
