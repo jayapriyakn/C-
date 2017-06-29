54c6efd8e84389ea8f7a8deb9b3b9b511f9956fa
jayapriyakn <jaya.priya1987kn@gmail.com>
2017-06-29 17:04:04 +0530

    negposzero.cpp

---
+++ negposzero.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+void main()
+{
+int no;
+cout<<"Enter a number";
+cin>>no;
+if(no<=0)
+{
+if(no==0)
+cout<<"The number is 0";
+else
+cout<<"The number is negative";
+}
+else
+cout<<"The number is positive";
+return 0;
+}
