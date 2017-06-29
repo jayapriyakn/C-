bd5d77acb5bab1fe9c2523cd6554636a7e13b234
jayapriyakn <jaya.priya1987kn@gmail.com>
2017-06-29 14:55:25 +0530

    vowelorconsonant.cpp

---
+++ vowelorconsonant.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+using namespace std;
+void main()
+{
+char c;
+int lc,uc;
+cout<<"Enter a alphabet";
+cin>>c;
+lc=(c=='a' ||c=='e' || c=='i' || c=='o' || c=='u');
+uc=(c=='A' ||c=='E' || c=='I' || c=='O' || c=='U');
+if(lc||up)
+cout<<c<<"is a vowel";
+else
+cout<<c<<"it is not a consonant";
+return 0;
+}
