cc396c95ae3f1dd562390feba1d20fa60662dfb8
jayapriyakn <jaya.priya1987kn@gmail.com>
2017-06-29 14:12:29 +0530

    largestno.cpp

--- largestno.cpp
+++ largestno.cpp
@@ -6,4 +6,17 @@ cout<<"Enter the three numbers";
 cin>>a;
 cin>>b;
 cin>>c:
-if(a>=
+if(a>=b && a>=c)
+{
+cout<<"a is greater"<<a;
+}
+if(b>=c && b>=c)
+{
+cout<<"b is greater"<<b;
+}
+if(c>=a && c>=a)
+{
+cout<<"c is greater"<<c;
+}
+  getch();
+}
