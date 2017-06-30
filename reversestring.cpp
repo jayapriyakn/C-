fa25c36926c443a70353a8a386a851e18e610ad2
jayapriyakn <jaya.priya1987kn@gmail.com>
2017-06-30 16:13:06 +0530

    reverse.cpp

--- reversestring.cpp
+++ reversestring.cpp
@@ -8,4 +8,18 @@ char str[100,tmp;
 int i,j=0;
 clrscr();
 gets(str);
-
+i=0;
+j=strlen(str)-1;
+while(i<j)
+{
+  temp=str[i];
+  str[i]=str[j];
+  str[j]=temp;
+  i++;
+  j--;
+}
+         cout<<"Reverse string is"<<str;
+         getch();
+         }
+           
+           
